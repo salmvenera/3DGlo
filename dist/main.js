2b8e82cd46ab1ad3bb7883fef89672cd49ba899d
salmvenera <ahmetovavm@gmail.com>
2021-10-30 14:09:33 +0500

    18

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("one Module"),console.log("two Module")})();
---
+++ src/modules/two.js
@@ -0,0 +1,6 @@
+const moduleLes = () => {
+    const variabl = 'two Module'
+    console.log(variabl);
+}
+
+export default moduleLes
