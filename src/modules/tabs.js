8b580cb9f7a95d4bb08db89d4b779fe42ba2e3ab
salmvenera <ahmetovavm@gmail.com>
2021-11-06 20:19:26 +0500

    не готово

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -2,9 +2,23 @@ const tabs = () => {
     const tabPanel = document.querySelector('.service-header')
     const tabs = document.querySelectorAll('.service-header-tab')
     const tabContent = document.querySelectorAll('.service-tab')
-console.log(tabPanel);
-console.log(tabs);
-console.log(tabContent);
+
+    tabPanel.addEventListener('click', (e) => {
+        if (e.target.closest('.service-header-tab')) {
+            const tabBtn = e.target.closest('.service-header-tab')
+            console.log(tabBtn);
+
+            tabs.forEach((tab, index) => {
+                if(tab === tabBtn) {
+                    tab.classList.add('active')
+                    tabContent[index].classList.remove('d-none')
+                } else {
+                    tab.classList.remove('active')
+                    tabContent[index].classList.add('d-none')
+                }
+            })
+        }
+    })
 
 } 
 export default tabs
